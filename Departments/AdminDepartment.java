2ae9993f25e61f8f2f74202a87a478deb2c530c6
GitHub-User0001 <107647723+GitHub-User0001@users.noreply.github.com>
2022-07-03 21:18:20 +0530

    Add files via upload

---
+++ Departments/AdminDepartment.java
@@ -0,0 +1,35 @@
+package Departments;
+
+
+public class AdminDepartment extends SuperDepartment {
+    
+	public AdminDepartment() 
+	{ 
+	
+	}
+	@Override
+	public String departmentName()
+	{
+		return "Admin Department";
+	}
+	
+	@Override
+	public String getTodaysWork()
+	{
+		return "Complete your documents Submission";
+	}
+	
+	@Override
+	public String getWorkDeadline()
+	{
+		return "Complete by EOD";
+	}
+		
+	@Override
+	public String isTodayAHoliday()
+	{
+		return "Today is not a Holiday";
+	}
+	 
+
+}
---
+++ Departments/HrDepartment.java
@@ -0,0 +1,41 @@
+package Departments;
+
+public class HrDepartment extends SuperDepartment {
+      
+	public HrDepartment() 
+	{
+		
+	}
+	
+	@Override
+	public String departmentName()
+	{
+		return "HR Department";
+	}
+	
+	@Override
+	public String getTodaysWork()
+	{
+		return "Fill today's timesheet and mark your attendance";
+	}
+	
+	@Override
+	public String getWorkDeadline()
+	{
+		return "Complete by EOD";
+	}
+	
+	public String doActivity()
+	{
+		return "team Lunch";
+	}
+	
+	@Override
+	public String isTodayAHoliday()
+	{
+		return "Today is not a Holiday";
+	}
+	
+	
+	
+}
